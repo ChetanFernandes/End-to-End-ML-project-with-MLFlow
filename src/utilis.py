20f361a3f884a0c33cac7663ab92a7cc36b06156
Chetan <chetanfern@gmail.com>
2024-12-27 10:50:19 +0530

    Update path

--- src/utilis.py
+++ src/utilis.py
@@ -35,7 +35,7 @@ def upload_data_db():
             client = MongoClient(URL)
             db = client['phishing']
             collection = db['phishing_collection']
-            df = pd.read_csv("notebook\data\Phishing.csv".rstrip("\\"))
+            df = pd.read_csv("app\notebook\data\Phishing.csv")
             data = df.to_dict('records')
             collection.insert_many(data)
             logging.info("CSV uploaded successfully")
